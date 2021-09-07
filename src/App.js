6db067dcfef1f493d5caafee97aea8e08e7690d0
Luiscerda <luiskrlos00231@gmail.com>
2021-09-07 11:58:49 -0500

    delete and update with firebase

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 import React, {useState, useEffect} from "react"
 import { isEmpty,size } from "lodash";
-import { addDocument, getCollection } from "./actions";
+import { addDocument, deleteDocument, getCollection, updateDocument } from "./actions";
 
 function App() {
   const [task, setTask] = useState("")
@@ -42,19 +42,29 @@ function App() {
     setTasks([...tasks,{ id: result.data.id, name: task}])
     setTask("")
   }
-  const saveTask = (e) => {
+  const saveTask = async(e) => {
     e.preventDefault()
     if (!validForm()) {
       return 
     }
 
+    const result = await updateDocument("tasks", id, {name: task})
+    if (!result.statusResponse) {
+      setError(result.error)
+      return
+    }
     const editedTasks = tasks.map(item => item.id === id ? {id, name : task} : item)
     setTasks(editedTasks)
     seteditMode(false)
     setid("")
     setTask("")
   }
-  const deleteTask = (id) => {
+  const deleteTask = async(id) => {
+    const result = await deleteDocument("tasks", id)
+    if (!result.statusResponse) {
+      setError(result.error)
+      return
+    }
     const filteredTasks = tasks.filter(task => task.id !== id)
     setTasks(filteredTasks)
   }
